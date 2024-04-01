1a4eabe7cee497579da97f30820f2e30f18f6d29
Debaprasad Sahoo <debaprasadworld@gmail.com>
2024-04-01 10:00:09 +0530

    slice code

--- main.go
+++ main.go
@@ -13,14 +13,17 @@ func main() {
 	fmt.Printf("We have %v tickets available and %v tickets remaining\n", conferenceTickets, remainingTickets,)
 	fmt.Println("Get your tickets to attend")
 
-	// when we know the values of the array
-	// var bookings = [50]string{"John", "Deba"}
+	// when we know the values of the slice
+	// var bookings = []string{"John", "Deba"}
 
-	// when we don't know the values of the array
-	// var bookings = [50]string{}
+	// when we don't know the values of the slice
+	// var bookings = []string{}
 
-	// alternate way to declare an array
-	var bookings [50]string
+	// alternate way to declare a slice
+	// var bookings []string
+
+	// alternate way to declare a slice
+	bookings := []string{}
 
 	var fName string
 	var lName string
@@ -40,14 +43,14 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets -= userTickets
-	bookings[0] = fName + " " + lName
+	bookings = append(bookings, fName + " " + lName)
 
-	fmt.Printf("The Whole Array: %v\n", bookings)
-	fmt.Printf("The first value of the Array: %v\n", bookings[0])
-	fmt.Printf("Array type: %T\n", bookings)
-	fmt.Printf("Array length: %v\n", len(bookings))
+	fmt.Printf("The Whole Slice: %v\n", bookings)
+	fmt.Printf("The first value of the Slice: %v\n", bookings[0])
+	fmt.Printf("Slice type: %T\n", bookings)
+	fmt.Printf("Slice length: %v\n", len(bookings))
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", fName, lName, userTickets, email)
+	fmt.Printf("Thank you %v for booking %v tickets. You will receive a confirmation email at %v.\n", bookings[0], userTickets, email)
 	fmt.Printf("Remaining tickets for %v are %v\n", conferenceName, remainingTickets)	
 
 }
