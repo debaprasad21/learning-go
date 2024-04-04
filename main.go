ab2b6b02449cd80f2893540316d7eb0912a69f22
Debaprasad Sahoo <debaprasadworld@gmail.com>
2024-04-04 15:46:04 +0530

    receiver function code

--- main.go
+++ main.go
@@ -1,38 +1,16 @@
 package main
 
-import "fmt"
-
 func main() {
-	 conferenceName := "Go Conference"
-	const conferenceTickets int = 50
-	var remainingTickets uint = 50
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
 
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+	cards.print()
 
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have %v tickets available and %v tickets remaining\n", conferenceTickets, remainingTickets,)
-	fmt.Println("Get your tickets to attend")
+	abc := deck{"abc", "def"}
+	abc.print()
 
-	var fName string
-	var lName string
-	var email string
-	var userTickets uint
-	// ask user to enter their name
-	fmt.Println("Enter first name:")
-	fmt.Scan(&fName)
+}
 
-	fmt.Println("Enter last name:")
-	fmt.Scan(&lName)
-
-	fmt.Println("Enter email name:")
-	fmt.Scan(&email)
-
-	fmt.Println("Enter no of tickets:")
-	fmt.Scan(&userTickets)
-
-	remainingTickets -= userTickets
-
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", fName, lName, userTickets, email)
-	fmt.Printf("Remaining tickets for %v are %v\n", conferenceName, remainingTickets)	
-
+func newCard() string {
+	return "Five of Diamonds"
 }
