07b8c1fa678e52b9810acd5ed56de3ef4c5f2451
Debaprasad Sahoo <debaprasadworld@gmail.com>
2024-04-04 15:51:01 +0530

    receiver function code

--- deck.go
+++ deck.go
@@ -6,6 +6,12 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+// the print function is a receiver function
+// it is a function that is attached to a type
+// in this case, the type is deck
+// the receiver function is called on a variable of type deck
+// the variable is available in the function as a variable called 'd'
+// the receiver function is called like this: cards.print()
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card, d)
