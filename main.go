0e17afa390c2d2219ebd44140e12e1be2aef41dc
Debaprasad Sahoo <debaprasadworld@gmail.com>
2024-04-07 01:38:54 +0530

    http

--- main.go
+++ main.go
@@ -1,79 +1,36 @@
 package main
 
-// bot is an interface that has a method getGreeting
-// getGreeting returns a string
-// englishBot and spanishBot are structs that implement the bot interface
-// printGreeting is a function that takes a bot interface and prints the greeting
-type bot interface {
-	// if you are a type in this program with a function called getGreeting and
-	// you return a string you are now an honorary member of type bot
-	getGreeting() string
-}
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
-// we use interfaces to define a contract between different types
-
-type englishBot struct{}
-type spanishBot struct{}
+type logWriter struct{}
 
 func main() {
-	eb := englishBot{}
-	sb := spanishBot{}
+	resp, err := http.Get("http://www.google.com")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
 
-	printGreeting(eb)
-	printGreeting(sb)
+	// bs := make([]byte, 99999) // Create a byte slice with 99999 elements
+	// resp.Body.Read(bs)        // Read the response body into the byte slice
+
+	// fmt.Println(string(bs)) // Print the response body
+
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
 
 }
 
-func printGreeting(b bot) {
-	println(b.getGreeting())
+// go through the video 67, 68, 69, 70, 71 of Udemy -> Go: The Complete Developer's Guide (Golang)
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("asasa", len(bs))
+	return len(bs), nil
 }
-
-func (englishBot) getGreeting() string {
-	// Very custom logic for generating an english greeting
-	return "Hi There!"
-}
-
-func (spanishBot) getGreeting() string {
-	return "Hola!"
-}
-
-/*
-	Interfaces are not generic types
-	Interfaces are 'implicit' - we don't manually have to say that our custom type satisfies some interface
-	 - hard to maintain which type satisfies which interface
-	Interfaces are a contract to help us manage types - Garbage in -> Garbage out.
-	 - If you don't implement the interface correctly, you will get an error
-	 - If our custom type's implementation of a function is broken, then interfaces wont help us!
-	Interfaces are tough, but very powerful
-	Interfaces are a way to make our code better
-
-	Example:
-		type bot interface {
-			getGreeting(string, int) (string, error)
-		}
-		bot -> interface name
-		getGreeting -> method name
-		(string, int) -> list of arguments
-		(string, error) -> list of return types
-
-	One Interfaces can include in many methods/functions
-	Example:
-		type bot interface {
-			getGreeting(string, int) (string, error)
-			printGreeting(string) (string, error)
-			getBotVersion() int
-			responseToUser(user) string
-		}
-*/
-
-/*
-	Concreate Type: Map, Struct, Int, String, EnglishBot, SpanishBot
-	 - We can create a value directly from a concreate type
-	 - built in types as well as our own custom types defined
-	Interface Type: bot
-	 - We can't create a value directly from an interface type
-
-	 Rules of interfaces:
-	 - We are not allowed to create a value directly from an interface type
-
-*/
