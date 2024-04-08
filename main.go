e604865d2a59fb2e45263d54709c665927d16214
Debaprasad Sahoo <debaprasadworld@gmail.com>
2024-04-08 09:30:22 +0530

    Adding debugger

--- main.go
+++ main.go
@@ -2,37 +2,48 @@ package main
 
 import "fmt"
 
+// we can debug the code by using fmt.Printf() to print the type of the variables
+// or adding log statements to see the flow of the code
+// logger is a package that is used to log the messages
+// delve is a debugger that is used to debug the code
+// delve link -> https://github.com/go-delve/delve/blob/master/Documentation/installation/osx/install.md
+// delve commands -> dlv
+// cmd+shift+p => mac command to open command palette
+// cntrl+shift+p => windows command to open command palette
+// Run and Debug in VSCode and create a launch.json file
+/* Update the program  as given below to debug from workspace
+ {
+  // Use IntelliSense to learn about possible attributes.
+  // Hover to view descriptions of existing attributes.
+  // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+  "version": "0.2.0",
+  "configurations": [
+    {
+      "name": "Launch Package",
+      "type": "go",
+      "request": "launch",
+      "mode": "auto",
+      "program": "${workspaceFolder}"
+    }
+  ]
+}
+*/
+// add breakpoints in the code by clicking on the left side of the line number
+// if u want to add breakpoints in the code by using command line use the below command
+// break ./main.go:15 -> 15 is the line number
+// ensure go.mod file is present in the project
+// launch the debugger from Run and Debug in VSCode
+
 func main() {
-	 conferenceName := "Go Conference"
-	const conferenceTickets int = 50
-	var remainingTickets uint = 50
+	count := 0
 
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+	for {
+		if count == 10 {
+			break
+		}
+		count++
 
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have %v tickets available and %v tickets remaining\n", conferenceTickets, remainingTickets,)
-	fmt.Println("Get your tickets to attend")
-
-	var fName string
-	var lName string
-	var email string
-	var userTickets uint
-	// ask user to enter their name
-	fmt.Println("Enter first name:")
-	fmt.Scan(&fName)
-
-	fmt.Println("Enter last name:")
-	fmt.Scan(&lName)
-
-	fmt.Println("Enter email name:")
-	fmt.Scan(&email)
-
-	fmt.Println("Enter no of tickets:")
-	fmt.Scan(&userTickets)
-
-	remainingTickets -= userTickets
-
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", fName, lName, userTickets, email)
-	fmt.Printf("Remaining tickets for %v are %v\n", conferenceName, remainingTickets)	
+	}
+	fmt.Println("Counter", count)
 
 }
